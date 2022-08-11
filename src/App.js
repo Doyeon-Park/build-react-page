17ef7778ca61acf102b2fe44af09f39bcae68d97
Doyeon Park (leah) <leah.park@momenti.tv>
2022-08-11 17:32:22 +0900

    final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
-
+import Header from "./Components/";
 function App() {
   return (
     <main style={style}>
-      <h1>Text</h1>
-      <ul>Hello,world!</ul> <h1>Card within the card</h1>
-      <ul>This is a card within a card</ul>{" "}
+      <section style={headerStyle}>
+        <h1>Text</h1>
+        <ul>Hello,world!</ul>
+      </section>
+
+      <section style={centerStyle}>
+        <h1>Card within the card</h1>
+        <ul>This is a card within a card</ul>
+        <section style={miniStyle}>
+          <h2>Inner card</h2>
+          <ul>This is an inner card</ul>
+        </section>
+      </section>
+
       <section style={sectionStyle}>
         <h1>Including image</h1>
         <img
@@ -26,8 +37,30 @@ const style = {
   maxWidth: "600px",
 };
 
+const headerStyle = {
+  border: "2px solid white",
+  margin: "20px",
+  padding: "10px",
+  boxShadow: "1px 2px 9px #a0a0a0",
+};
+
+const centerStyle = {
+  border: "2px solid white",
+  margin: "20px",
+  padding: "10px",
+  boxShadow: "1px 2px 9px #a0a0a0",
+};
+
+const miniStyle = {
+  border: "2px solid white",
+  margin: "20px",
+  padding: "5px",
+  boxShadow: "1px 2px 9px #a0a0a0",
+};
+
 const sectionStyle = {
-  border: "2px solid gray",
+  border: "2px solid white",
+  margin: "20px",
   padding: "10px",
   boxShadow: "1px 2px 9px #a0a0a0",
 };
--- src/Components/Topbanner.js
+++
@@ -1,1 +0,0 @@
-<h1 className="banner_box">React Starter</h1>;
